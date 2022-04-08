345bcec21db71e005d31a40507d636a8f8f3b03b
aya-mex <aya.yamada03@gmail.com>
2022-04-08 11:27:59 +0900

    パスワードのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+  
 end
