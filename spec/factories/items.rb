80e90b593ca8e4adc1533b90a44191d6c38a470c
aya-mex <aya.yamada03@gmail.com>
2022-04-11 17:55:31 +0900

    アソシエーション追記

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,7 +8,11 @@ FactoryBot.define do
     shipping_fee_id {Faker::Number.between(from: 2, to: 3)}
     prefecture_id {Faker::Number.between(from: 2, to: 48)}
     shipping_day_id {Faker::Number.between(from: 2, to: 4)}
-    price {Faker::Number.between(from: 300, to: 9999999)}
+    price {Faker::Number.between(from: 300, to: 9_999_999)}
     association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpeg'), filename: 'test_image.jpeg')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品が出品できる場合' do
+      it '全ての項目を正しく入力すれば出品できる' do
+      end
+    end
+    context '商品が出品できない場合' do
+      it '' do
+      end
+ end
