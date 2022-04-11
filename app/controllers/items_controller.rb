d8d29ccc9b2b3ec846395ac7c9d599a8e2426e82
aya-mex <aya.yamada03@gmail.com>
2022-04-11 10:59:05 +0900

    item_paramsを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,11 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :decsription, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+  
 end
