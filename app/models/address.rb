0bbbf6060e7342cf474c958013c3333170f0662f
aya-mex <aya.yamada03@gmail.com>
2022-04-14 11:31:04 +0900

    ActiveHash追記

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,7 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :order
+  belongs_to :prefecture
 
   validates :zip_code, :city, :street, :phone_number, presence: true
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
