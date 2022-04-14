ffc39ab6cf4d2523ed547005e5a70dfe3a0b49a3
aya-mex <aya.yamada03@gmail.com>
2022-04-14 11:29:30 +0900

    バリデーション追記

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :order
+
+  validates :zip_code, :city, :street, :phone_number, presence: true
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 end
