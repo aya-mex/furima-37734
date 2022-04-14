003ae2f547f0c0a49bb91f682ef85b4847e273d8
aya-mex <aya.yamada03@gmail.com>
2022-04-14 14:31:38 +0900

    バリデーション修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,6 +3,8 @@ class Address < ApplicationRecord
   belongs_to :order
   belongs_to :prefecture
 
-  validates :zip_code, :city, :street, :phone_number, presence: true
+  validates :zip_code, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
+  validates :city, :street, presence: true
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :phone_number, presence: true, format: { with: ^0\d{9,10}$, message: "is too short"}
 end
