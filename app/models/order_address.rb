dbb57c556647c873ffedc338618dd6244f28a21c
aya-mex <aya.yamada03@gmail.com>
2022-04-14 18:42:05 +0900

    バリデーション修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,7 +6,7 @@ class OrderAddress
     validates :zip_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city, :street, :user_id, :item_id
-    validates :phone_number, format: { with: ^0\d{9,10}$, message: "is too short"}
+    validates :phone_number, format: { with: ^0[0-9]{9,10}$, message: "is too short" }
   end
   
   def save
