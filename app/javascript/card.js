217f24f76f8e8d08e670984d0fe82e985643137b
aya-mex <aya.yamada03@gmail.com>
2022-04-15 17:37:48 +0900

    Revert "card.jp記述"
    This reverts commit 33b06227157fa31945e0b06479e5597cedddf185.

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,35 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_1becd7d7d1f708d31952d067");
-  const submit = document.getElementById("button");
-  submit.addEventListener("click", (e) => {
-    e.preventDefault();
-    
-    const formResult = document.getElementById("charge-form");
-    const formData = new FormData(formResult);
-
-    const card = {
-      number: formData.get("order_address[number]"),
-      exp_month: formData.get("order_address[exp_month]"),
-      exp_year: `20${formData.get("order_address[exp_year]")}`,
-      cvc: formData.get("order_address[cvc]")
-    };
-
-    Payjp.createToken(card, (status, response) => {
-      if (status ==200) {
-        const token = response.id;
-        const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden">`;
-        renderDom.insertAdjacentHTML("beforeend", tokenObj);
-      }
-
-      document.getElementById("order_address_number").removeAttribute("name");
-      document.getElementById("order_address_exp_month").removeAttribute("name");
-      document.getElementById("order_address_exp_year").removeAttribute("name");
-      document.getElementById("order_address_cvc").removeAttribute("name");
-
-      document.getElementById("charge-form").submit();
-    });
-  });
+  console.log("カード情報トークン化のためのJavaScript");
 };
 
 window.addEventListener("load", pay);
