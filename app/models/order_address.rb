28cc60d26c32224b64b25a986911341c4dd398f5
aya-mex <aya.yamada03@gmail.com>
2022-04-15 20:10:00 +0900

    バリデーション修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,13 +2,13 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :zip_code, :prefecture_id, :city, :street, :building, :phone_number, :user_id, :item_id, :token
 
-  validates :token, presence: true
-  validates :zip_code, presence: true,
-                       format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :city, :street, :user_id, :item_id, presence: true
-  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: 'is too short' },
-                           numericality: { message: 'is invalid. Input only number' }
+  with_options presence: true do
+    validates :token, :city, :street, :user_id, :item_id
+    validates :zip_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is too short' },
+                             numericality: { message: 'is invalid. Input only number' }
+  end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
