d50e2f4d005b0ad3c69835c66b2f230e6107e319
aya-mex <aya.yamada03@gmail.com>
2022-04-15 12:04:22 +0900

    params修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,12 +2,10 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :zip_code, :prefecture_id, :city, :street, :building, :phone_number, :user_id, :item_id
 
-  with_options presence: true do
-    validates :zip_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
-    validates :city, :street, :user_id, :item_id
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is too short" }
-  end
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :zip_code, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city, :street, :user_id, :item_id, presence: true
+    validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only number" }
   
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
