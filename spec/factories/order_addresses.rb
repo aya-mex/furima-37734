ae904e1b4c0b75519c35227cb336941ffc546b44
aya-mex <aya.yamada03@gmail.com>
2022-04-15 12:47:15 +0900

    FactoryBot設定

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_address do
+    zip_code { '123-4567' }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    city { '港区' }
+    street { '青山' }
+    building { '青山ビル' }
+    phone_number { '09012345678' }
+  end
+end
