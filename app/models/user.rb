6daa4a946ed174fef04d48371d543862ddc37058
aya-mex <aya.yamada03@gmail.com>
2022-04-15 20:10:29 +0900

    userモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  has_many :order
+  has_many :orders
 
   validates :nickname, :birthday, presence: true
   validates :password,
